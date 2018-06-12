854fe1f898cf2d8eebf5cc3557a161711ab83e2f
Eoin Murphy <eoin.murphy@traveltechlabs.com>
2018-06-12 22:14:03 +0100

    Fix broken tests

--- test.js
+++ test.js
@@ -6,26 +6,26 @@ import execa from 'execa'
 test('throw with no exported func calling only the file', async t => {
 	const error = await t.throws(execa('node', ['./testfiles/noexport.spec.js']))
 	const msg = `Error: RUNNABLE-EXPORTS: can't run your command: `
-  t.true(error.message.search(msg) > -1)
+  t.true(error.stderr.search(msg) > -1)
 
 })
 
 test('throw with default exported func calling with double type args', async t => {
 	const error = await t.throws(execa('node', ['./testfiles/defaultexport.spec.js', 'asd', '--asd']))
 	const msg = `Error: RUNNABLE-EXPORTS: can't run with both objectArgs and arrayArgs: {\"asd\":true}, asd`
-  t.true(error.message.search(msg) > -1)
+  t.true(error.stderr.search(msg) > -1)
 })
 
 test('throw with exported func calling with a wrong name', async t => {
 	const error = await t.throws(execa('node', ['./testfiles/export.spec.js', 'asd']))
 	const msg = `Error: RUNNABLE-EXPORTS: can't run your command: asd`
-  t.true(error.message.search(msg) > -1)
+  t.true(error.stderr.search(msg) > -1)
 })
 
 test('throw with exported func calling with double type args', async t => {
 	const error = await t.throws(execa('node', ['./testfiles/export.spec.js', 'test', 'asdasd', '--asd']))
 	const msg = `Error: RUNNABLE-EXPORTS: can't run with both objectArgs and arrayArgs: {\"asd\":true}, asdasd`
-  t.true(error.message.search(msg) > -1)
+  t.true(error.stderr.search(msg) > -1)
 })
 
 test('work with default export (no-args)', async t => {
@@ -58,4 +58,3 @@ test('nested runnable-exports', async t => {
 	const stdout = (await execa('node', ['./testfiles/nested.spec.js', 'test', '--asd'])).stdout
 	t.is(stdout, 'cache deleted')
 })
-
